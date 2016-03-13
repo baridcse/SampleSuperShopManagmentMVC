05233d5e8a722150eb784942d7a5ab1865070c72
Ajharul Islam Barid <Ajharul Islam Barid>
2016-03-13 15:50:54 +0600

    First

---
+++ shopManagment/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace shopManagment
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.config/{*pathInfo}");
+
+           routes.MapRoute(
+                 name: "Default1",
+                 url: "",
+                 defaults: new { controller = "Login", action = "LoginView", id = UrlParameter.Optional }
+             );
+             
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ shopManagment/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shopManagment.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+      
+        public ActionResult LoginView()
+        {
+            return View("Login");
+        }
+        public ActionResult LoginCheek(string UsernameTextbox, string PasswordTextbox)
+        {
+
+            string name = UsernameTextbox;
+            string password= PasswordTextbox;
+
+            if (name == "barid" && password == "123")
+            {
+                return RedirectToAction("sell", "Home");
+            }
+            else
+            {
+                ViewData["isWrong"] = "true";
+                return View("Login");
+            }
+
+        }
+    }
+}
